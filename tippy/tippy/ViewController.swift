9126af4f85368432adad3889d9ea0678c1a1ac84
JonLuca DeCaro <jdecaro@usc.edu>
2016-09-13 21:24:50 -0700

    Tip Calculator

--- tippy/tippy/ViewController.swift
+++ tippy/tippy/ViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        super.didReceiveMemoryWarning()s
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true);
+    }
+    @IBAction func calculateTip(sender: AnyObject) {
+        let tipPercentages = [0.18,0.2,0.25];
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * 0.2
+        let total = bill + tip
+        
+        
+        tipLabel.text = String(format: "$%.2f", tip);
+        totalLabel.text = String(format: "$%.2f", total);    }
 
 }
 
